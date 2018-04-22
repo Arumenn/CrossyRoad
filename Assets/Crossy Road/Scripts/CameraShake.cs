86e2dd94d163dd81a4cb4ce8b6d95091d2de3d1d
arumenn <abel.laflamme@gmail.com>
2018-04-22 12:15:08 -0400

    Camera shake

---
+++ Assets/Crossy Road/Scripts/CameraShake.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public float jumpIteration = 4.5f;
+    
+    public void Shake()
+    {
+        float height = Mathf.PerlinNoise(jumpIteration, 0) * 5f;
+        height = height * height * 0.2f;
+
+        float shakeAmount = height; //degrees to shake the camera
+        float shakePeriodTime = 0.25f; //period of each shake
+        float dropOffTime = 1.25f; //time before settling down
+
+        LTDescr shakeTween = LeanTween.rotateAroundLocal(gameObject, Vector3.right, shakeAmount, shakePeriodTime).setEase(LeanTweenType.easeShake).setLoopClamp().setRepeat(-1);
+
+        LeanTween.value(gameObject, shakeAmount, 0, dropOffTime).setOnUpdate((float val) => { shakeTween.setTo(Vector3.right * val); }).setEase(LeanTweenType.easeInOutQuad);
+
+        //I have no idea what i'm doing.
+    }
+}
