c6c76d6de875779f88df8cc82b5fcb5e86beb718
arumenn <abel.laflamme@gmail.com>
2018-04-29 16:14:23 -0400

    Highscore

--- Assets/Crossy Road/Scripts/Manager.cs
+++ Assets/Crossy Road/Scripts/Manager.cs
@@ -9,6 +9,8 @@ public class Manager : MonoBehaviour
     [Header("GUI")]
     public Text uiCoin = null;
     public Text uiDistance = null;
+    public Text uiCoinHighscore = null;
+    public Text uiDistanceHighscore = null;
     public Camera _camera = null;
     public GameObject uiStartScreen = null;
     public GameObject uiGameOver = null;
@@ -25,6 +27,8 @@ public class Manager : MonoBehaviour
     private int currentCoins = 0;
     private int currentDistance = 0;
     private bool canPlay = false;
+    private int highscoreDistance = 0;
+    private int highscoreCoins = 0;
 
     private static Manager s_Instance;
     public static Manager GetInstance
@@ -42,6 +46,11 @@ public class Manager : MonoBehaviour
 
     private void Start()
     {
+        highscoreDistance = PlayerPrefs.GetInt("HighscoreDistance", 0);
+        highscoreCoins = PlayerPrefs.GetInt("HighscoreCoins", 0);
+        uiDistanceHighscore.text = highscoreDistance.ToString();
+        uiCoinHighscore.text = highscoreCoins.ToString();
+
         levelGenerator.SetupNewLevel();
         for (int i = 0; i < levelCount; i++)
         {
@@ -70,12 +79,20 @@ public class Manager : MonoBehaviour
     {
         currentCoins += value;
         uiCoin.text = currentCoins.ToString();
+        if (currentCoins > highscoreCoins)
+        {
+            uiCoinHighscore.text = currentCoins.ToString();
+        }
     }
 
     public void UpdateDistanceCount()
     {
         currentDistance++;
         uiDistance.text = currentDistance.ToString();
+        if (currentDistance > highscoreDistance)
+        {
+            uiDistanceHighscore.text = currentDistance.ToString();
+        }
 
         levelGenerator.RandomGenerator();
     }
@@ -97,6 +114,15 @@ public class Manager : MonoBehaviour
         _camera.GetComponent<CameraShake>().Shake();
         _camera.GetComponent<CameraFollow>().enabled = false;
         GuiGameOver();
+        //saves highscore if necessary
+        if (currentDistance > highscoreDistance)
+        {
+            PlayerPrefs.SetInt("HighscoreDistance", currentDistance);
+        }
+        if (currentCoins > highscoreCoins)
+        {
+            PlayerPrefs.SetInt("HighscoreCoins", currentCoins);
+        }
     }
 
     private void GuiGameOver()
